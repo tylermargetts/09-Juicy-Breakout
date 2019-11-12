049e0522ed4670c66ccb0024aba1e4ab48006282
Tyler Margetts <tmargett@iu.edu>
2019-11-11 23:51:43 -0500

    Create World.gd

---
+++ Scenes/World.gd
@@ -0,0 +1,16 @@
+extends Node
+
+export var score = 0
+export var lives = 3
+
+func increase_score(s):
+ score += int(s)
+ find_node("Score").update_score()
+ if score >= 18:
+  get_tree().change_scene("res://Scenes/Win.tscn")
+ 
+func decrease_lives():
+ lives -= 1
+ find_node("Lives").update_lives()
+ if lives <= 0:
+  get_tree().change_scene("res://Scenes/End.tscn")
